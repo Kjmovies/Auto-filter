aa925b44c214328cfc1ed3bf6f26ac71c74af5ca
Shiva7774 <156524764+Shiva7774@users.noreply.github.com>
2024-05-16 20:40:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://javalekiran1:L3G6ziKk3bvuW77U@cluster0.odqwmk8.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kiranjavale8888:6jHGENuHHOnNj6Vr@cluster0.yo8wsdl.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "bisal-ki-bot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
